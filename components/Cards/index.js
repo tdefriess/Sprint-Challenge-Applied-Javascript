cf8403f7c5d23ce4bcc3e42bda9d16b35e6e1fe9
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-17 11:55:21 -0500

    Topic list complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,12 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// axios
+// .get('https://lambda-times-backend.herokuapp.com/articles')
+// .then(response => {
+//   console.log(response);    
+// })
+// .catch( error => {
+//   console.log('The data was not returned', error);
+// })
