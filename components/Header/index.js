55f6389445d31d297f64316bcd1c9ad4bb467646
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-17 11:11:47 -0500

    Completed header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,5 +10,24 @@
 // And add it to the DOM in the .header-container component
 
 function Header() {
-    
+    const header = document.createElement('div'),
+        spanOne = document.createElement('span'),
+        title = document.createElement('h1'),
+        spanTwo = document.createElement('span');
+
+    header.classList.add('header');
+    spanOne.classList.add('date');
+    spanTwo.classList.add('temp');
+
+    spanOne.textContent = 'SMARCH 28, 2019';
+    title.textContent = 'Lambda Times';
+    spanTwo.textContent = '98°';
+
+    header.append(spanOne);
+    header.append(title);
+    header.append(spanTwo);
+
+    return header;
 }
+
+document.querySelector('.header-container').append(Header());
